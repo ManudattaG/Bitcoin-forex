72037f17d12064528228904e4daa1fceaa97159e
Manudatta G <manudatta4u@gmail.com>
2020-11-05 13:01:47 +0530

    Bitcoin service v1.1

--- tests/test_bitcoin.py
+++ tests/test_bitcoin.py
@@ -1,102 +1,122 @@
 import datetime
 import pytest
 from decimal import Decimal
-from forex_python.bitcoin import (get_latest_price, get_previous_price_list, BtcConverter)
+from forex_python.bitcoin import (get_latest_price, get_previous_price, get_previous_price_list, BtcConverter)
+from forex_python.converter import RatesNotAvailableError
+from src import bitcoin
+import flask
+from flask import jsonify
+from flask_restful import Api
 
-class TestBitCoinForceDecimal:
-    """
-    Test Perfect fit - get latest price with force_decimal flag
-    """
-    def test_bitcoin_with_force_decimal(self):
-        btc_obj = BtcConverter(force_decimal=True)
-        price = btc_obj.get_latest_price('USD')
-        assert type(price) == Decimal
+app = flask.Flask(__name__)
+api = Api(app)
 
-    def test_bitcoin_without_force_decimal(self):
-        btc_obj = BtcConverter()
-        price = btc_obj.get_latest_price('USD')
+"""
+Test cases of Bitcoin-forex service developed for 2 REST APIs
+"""
+
+@app.route('/api/getLatestRate')
+def getLatestRate():
+    return jsonify(latest_rate=14332.2183)
+
+def test_api_getLatestRate():
+    with app.test_request_context("/api/getLatestRate"):
+        app.preprocess_request()
+        assert flask.request.path == '/api/getLatestRate'
+    with app.test_client() as c:
+        res = c.get('/api/getLatestRate')
+        assert res.json == {'latest_rate': 14332.2183}
+
+
+@app.route('/api/getHistoricalRates')
+def getHistoricalRates():
+    historical_rates = {
+        "historical_rates": {
+            "2020-10-15": 11510.5367, 
+            "2020-10-16": 11325.5217, 
+            "2020-10-17": 11369.9133, 
+            "2020-10-18": 11516.9667, 
+            "2020-10-19": 11758.5467, 
+            "2020-10-20": 11922.975, 
+            "2020-10-21": 12811.4867, 
+            "2020-10-22": 12987.9017, 
+            "2020-10-23": 12940.1067, 
+            "2020-10-24": 13127.055, 
+            "2020-10-25": 13039.0133
+        }
+    }
+    return(jsonify({"historical_rates": historical_rates}))
+
+def test_api_getHistoricalRates():
+    with app.test_request_context("/api/getHistoricalRates?startDate=2020-10-15&endDate=2020-10-25"):
+        app.preprocess_request()
+        assert flask.request.path == '/api/getHistoricalRates'
+        assert flask.request.args['startDate'] == '2020-10-15'
+        assert flask.request.args['endDate'] == '2020-10-25'
+    with app.test_client() as c:
+        res = c.get('/api/getHistoricalRates?startDate=2020-10-15&endDate=2020-10-25')
+        historical_rates = {
+            "historical_rates": {
+                "2020-10-15": 11510.5367, 
+                "2020-10-16": 11325.5217, 
+                "2020-10-17": 11369.9133, 
+                "2020-10-18": 11516.9667, 
+                "2020-10-19": 11758.5467, 
+                "2020-10-20": 11922.975, 
+                "2020-10-21": 12811.4867, 
+                "2020-10-22": 12987.9017, 
+                "2020-10-23": 12940.1067, 
+                "2020-10-24": 13127.055, 
+                "2020-10-25": 13039.0133
+            }
+        }
+        assert res.json == {'historical_rates': historical_rates}
+
+
+"""
+Additional Test cases of bitcoin wrapper service from "forex_python"
+"""
+
+class TestLatestPrice():
+    """
+    Test get latest price using currency code
+    """
+    def test_latest_price_valid_currency(self):
+        price = get_latest_price('USD')
         assert type(price) == float
 
-class TestBitcoinLatestRate:
-    """
-    Test Perfect fit - get latest price using currency code
-    """
-    @pytest.mark.parametrize(
-        "currency, expected, exception",
-        [
-            ("USD", float, None),
-            ("BTC", int, None),
-            ("EUR", float, None)
-        ]
-    )
-    def test_latest_price_valid_codes(self, currency, expected, exception):
-        if exception:
-            with pytest.raises(exception):
-                result = get_latest_price(currency=currency)
-        else:
-            result = get_latest_price(currency=currency)
-            assert type(result) == expected
+    def test_latest_price_invalid_currency(self):
+        price = get_latest_price('XYZ')
+        assert price == None
 
+class TestPreviousPrice():
     """
-    Test edge(failure) cases - get latest price using currency code
+    Test Price with date input
     """
-    @pytest.mark.parametrize(
-        "currency, expected, exception",
-        [
-            ("XYZ", None, None),
-            ("", None, None),
-            (250, None, None),
-            ("100", None, None),
-            (["USD"], None, None)
-        ]
-    )
-    def test_latest_price_invalid_codes(self, currency, expected, exception):
-        if exception:
-            with pytest.raises(exception):
-                result = get_latest_price(currency=currency)
-        else:
-            result = get_latest_price(currency=currency)
-            assert result is None
+    def test_previous_price_valid_currency(self):
+        date_obj = datetime.datetime.today() - datetime.timedelta(days=15)
+        price = get_previous_price('USD', date_obj)
+        assert type(price) == float
 
-class TestBitcoinHistoricalRates:
-    """
-    Test Perfect fit - get historical price using currency code
-    """
-    @pytest.mark.parametrize(
-        "startDate, endDate, currency, expected, exception",
-        [
-            (datetime.datetime.today() - datetime.timedelta(days=10), datetime.datetime.today(), "USD", dict, None),
-            (datetime.datetime.today() - datetime.timedelta(days=5), datetime.datetime.today(), "BTC", dict, None),
-            (datetime.datetime.today() - datetime.timedelta(days=6), datetime.datetime.today(), "EUR", dict, None),
-            (datetime.datetime.today(), datetime.datetime.today(), "USD", dict, None),
-            (datetime.datetime.today() - datetime.timedelta(days=int("10")), datetime.datetime.today(), "INR", dict, None)
-        ]
-    )
-    def test_historical_price_valid_codes(self, startDate, endDate, currency, expected, exception):
-        if exception:
-            with pytest.raises(exception):
-                result = get_previous_price_list(currency=currency, start_date=startDate, end_date=endDate)
-        else:
-            result = get_previous_price_list(currency=currency, start_date=startDate, end_date=endDate)
-            assert type(result) == expected
+    def test_previous_price_invalid_currency(self):
+        date_obj = datetime.datetime.today() - datetime.timedelta(days=15)
+        with pytest.raises(RatesNotAvailableError):
+            get_previous_price('XYZ', date_obj)
 
+class TestPreviousPriceList():
     """
-    Test edge(failure) cases - get historical price using currency code
+    Test previous price list for a currency
     """
-    @pytest.mark.parametrize(
-        "startDate, endDate, currency, expected, exception",
-        [
-            ("2020-10-15", "2020-10-25", "USD", "", AttributeError),
-            ("2020-10-20", "2020-10-25", "XYZ", "", AttributeError),
-            ("2020-10-01", "2020-10-05", "INR", "", AttributeError),
-            (datetime.datetime.today() - datetime.timedelta(days=10), datetime.datetime.today(), "XYZ", dict, None),
-            (datetime.datetime.today() - datetime.timedelta(days=-2), datetime.datetime.today(), "USD", dict, None)
-        ]
-    )
-    def test_historical_price_invalid_codes(self, startDate, endDate, currency, expected, exception):
-        if exception:
-            with pytest.raises(exception):
-                result = get_previous_price_list(currency=currency, start_date=startDate, end_date=endDate)
-        else:
-            result = get_previous_price_list(currency=currency, start_date=startDate, end_date=endDate)
-            assert type(result) == expected
+    def test_previous_price_list_with_valid_currency(self):
+        start_date = datetime.datetime.today() - datetime.timedelta(days=15)
+        end_date = datetime.datetime.today()
+        price_list = get_previous_price_list('USD', start_date, end_date)
+        assert price_list
+        assert type(price_list) == dict
+
+    def test_previous_price_list_with_invalid_currency(self):
+        start_date = datetime.datetime.today() - datetime.timedelta(days=15)
+        end_date = datetime.datetime.today()
+        price_list = get_previous_price_list('XYZ', start_date, end_date)
+        assert price_list == {} # returns empty dict
+        assert type(price_list) == dict
